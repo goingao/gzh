0ed3ffafd194450d768bc4b94c98303f519f1c68
goingao <goingao@163.com>
2020-04-11 16:27:45 +0800

    logback 自定义配置

--- gzh-web/src/main/java/com/goingao/gzh/web/goods/controller/GoodsController.java
+++ gzh-web/src/main/java/com/goingao/gzh/web/goods/controller/GoodsController.java
@@ -1,5 +1,7 @@
 package com.goingao.gzh.web.goods.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GoodsController {
 
+    Logger logger = LoggerFactory.getLogger(GoodsController.class);
+
     @GetMapping("/list")
     public String list() {
+        logger.trace("trace 这是商品列表");
+        logger.debug("debug 这是商品列表");
+        logger.info("info 这是商品列表");
+        logger.warn("warn 这是商品列表");
+        logger.error("error 这是商品列表");
         return "goods list";
     }
 }
